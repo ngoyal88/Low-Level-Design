730e64e7fee9b01a50b048abead59cdbc3363821
Nikhil Goyal <135103317+ngoyal88@users.noreply.github.com>
2025-10-15 23:38:20 +0530

    Add basic Logging_System.cpp with LogLevel class
    Introduces Logging_System.cpp containing a LogLevel class with static constants for DEBUG, INFO, and ERROR levels. Also adds a main function demonstrating usage.

---
+++ Logging_System.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class LogLevel {
+    public:
+    static const int DEBUG = 1;
+    static const int INFO = 2;
+    static const int ERROR = 3;
+
+    private:
+    
+};
+int main(){
+    LogLevel log = LogLevel::DEBUG;
+    return 0;
+}
